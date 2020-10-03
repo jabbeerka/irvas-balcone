94a5f3b2634c826c6a99b5610cd9a0524919c6cb
J.Kholikov <jabbeerka@gmail.com>
2020-10-03 23:46:29 +0300

    add AJAX request and tabs

---
+++ src/js/forms.js
@@ -0,0 +1,59 @@
+import { event } from "jquery";
+
+const forms = () => {
+    const form = document.querySelectorAll('form'),
+        inputs = document.querySelectorAll('input'),
+        phoneInputs = document.querySelectorAll('input[name="user_phone"]');
+
+    phoneInputs.forEach(item => {
+        item.addEventListener('input', () => {
+            item.value = item.value.replace(/\D/,"");
+        });
+    });
+
+    const messages = {
+        loading: 'Загрузка...',
+        success: 'Спасибо, скоро мы с вами свяжемся',
+        failure: 'Что-то пошло не так!!'
+    };
+    const postData = async (url, data) => {
+        document.querySelector('.status').textContent = messages.loading;
+        let res = await fetch(url, {
+            method: "POST",
+            body: data
+        });
+        return await res.text();
+    };
+    const clearInputs = () => {
+        inputs.forEach(item => {
+            item.value = "";
+        });
+    };
+
+    form.forEach(item => {
+        item.addEventListener('submit', (e) => {
+            e.preventDefault();
+        let statusMessage = document.createElement('div');
+        statusMessage.classList.add('status');
+        item.appendChild(statusMessage);
+        
+        const formData = new FormData(item);
+        postData('assets/server.php', formData)
+            .then(res => {
+                console.log(res);
+                statusMessage.textContent = messages.success;
+            }).catch(() => {
+                statusMessage.textContent = messages.failure;
+            }).finally(() => {
+                clearInputs();
+                setTimeout(() => {
+                    statusMessage.remove();
+                }, 5000);
+            });
+
+        });
+    });
+
+};
+
+export default forms;
--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,13 @@
 import './slider';
 import modals from './modals';
+import tabs from './tabs';
+import forms from './forms';
 
 
 document.addEventListener("DOMContentLoaded", ()=> {
+    'use-strict';
     modals();
+    tabs(".glazing_slider", ".glazing_block", ".glazing_content", "active");
+    tabs(".decoration_slider", ".no_click", ".decoration_content > div > div", "after_click");
+    forms();
 });
